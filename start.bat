97d067d01b2c8bf849dd58cd7ddc73776fe6cea6
Margo.Gediun <margarita.gediun@gmail.com>
2020-05-11 18:36:06 +0300

    Blog-first-modal

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-browser-sync start --server --files "css/*.css, css/media/*.css, css/features/*.css, css/home/*.css, *.html, js/*.js"
+browser-sync start --server --files "css/*.css, css/media/*.css, css/features/*.css, css/home/*.css, css/blog/*.css, *.html, js/*.js"
